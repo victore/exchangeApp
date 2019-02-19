9014479d12139dd68ff57e48b54b8d378ed4bb65
Victor Guadarrama <victor.gm@eucledian.com>
2019-02-18 23:15:36 -0600

    Exchange app v1 OK

---
+++ src/Exchange.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class Exchange extends Component {
+  constructor(props) {
+    super(props);
+
+    this.currencies = ["AUD", "CAD", "CHF", "INR", "USD", "EUR", "MXN", "GBP"]
+
+    this.state = {
+      base: "USD",
+      other: "EUR",
+      value: 0,
+      converted: 0
+    };
+  }
+
+  render() { 
+    return (
+      <div>
+        <div>
+          <select onChange={this.makeSelection} name="base" value={this.state.base}>
+            { this.currencies.map(currency => <option key={currency} value={currency}>{currency}</option>) }
+          </select>
+          <input value={this.state.value} onChange={this.changeValue} />
+        </div>
+        <div>
+          <select onChange={this.makeSelection} name="other" value={this.state.other}>
+            { this.currencies.map(currency => <option key={currency} value={currency}>{currency}</option>) }
+          </select>
+          <input disabled={true} value={this.state.converted === null ? "Calculating..." : this.state.converted} />
+        </div>
+      </div>
+    );
+  }
+
+  makeSelection = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }, this.recalculate)
+  }
+
+  changeValue = (event) => {
+    this.setState({
+      value: event.target.value,
+      converted: null
+    }, this.recalculate);
+  }
+
+  recalculate = () => {
+    const value = parseFloat(this.state.value);
+    if (isNaN(value)) {
+      return;
+    }
+
+    fetch(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          converted: data.rates[this.state.other] * value
+        });
+      });
+  }
+
+}
+
+export default Exchange;
